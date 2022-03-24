ffa6682e88eb18878afc042e8cf6fe609a5adede
Bigbentwo <21048789+Bigbentwo@users.noreply.github.com>
2022-03-24 23:22:59 +0100

    Some testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use sha3::{Digest, Sha3_256};
+
 fn main() {
-    println!("Hello world");
+    let mut hash = Sha3_256::new();
+    hash.update("data");
+    let hash: [u8; 32] = hash.finalize().as_slice().try_into().unwrap();
+    println!("{:?}", hash);
 }
