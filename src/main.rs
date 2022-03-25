dbe178f73e041cd601034a2800740b5b4f834ad3
Bigbentwo <21048789+Bigbentwo@users.noreply.github.com>
2022-03-25 12:27:04 +0100

    More Readme

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use sha3::{Digest, Sha3_256};
+use rand::prelude::*;
 
 fn main() {
     let mut hash = Sha3_256::new();
-    hash.update("data");
-    let hash: [u8; 32] = hash.finalize().as_slice().try_into().unwrap();
-    println!("{:?}", hash);
+    //hash.update("data");
+    let hash: Vec<u8> = hash.finalize()[..].to_vec();
+    for val in &hash {
+        print!("{:X}", val);
+    }
+    println!("");
+
+    let mut rng = rand_chacha::ChaCha20Rng::from_entropy();
+    println!("{}", rng.gen_range(0..10000));
+    println!("{:?}", rng.gen::<[u8; 32]>());
+
+    println!("{:?}", b"test");
 }
