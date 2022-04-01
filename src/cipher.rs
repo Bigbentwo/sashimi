3913cdc3ae2d12dea19e62f4a8828db8888297ef
Bigbentwo <21048789+Bigbentwo@users.noreply.github.com>
2022-04-01 22:11:39 +0200

    Started stream cipher

--- src/cipher.rs
+++ src/cipher.rs
@@ -1,4 +1,16 @@
-//use crate::hash;
+mod stream {
+    use crate::hash;
+    fn encrypt_in_place(
+        data: &[u8],
+        salt: &[u8; 64],
+        s_cost: usize,
+        t_cost: usize,
+    ) -> [u8; 64] {
+        let stream = hash::Sashimi::new();
+        let mut mask = ;
+        salt.clone()
+    }
+}
 
 #[cfg(test)]
 mod tests {
